a37eb408704ed708a477e3e91d3c8ab426b5b37c
Érik Eiji <eriknibe1@gmail.com>
2023-04-10 14:13:37 -0300

    feat: Tarefa 1 finalizada

---
+++ users/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from users.models import User
+from rest_framework.validators import UniqueValidator
+
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+
+    username = serializers.CharField(
+        max_length=150,
+        validators=[
+            UniqueValidator(User.objects.all(), message="username already taken.")
+        ],
+    )
+    password = serializers.CharField(max_length=127, write_only=True)
+
+    email = serializers.EmailField(
+        max_length=127,
+        validators=[
+            UniqueValidator(User.objects.all(), message="email already registered.")
+        ],
+    )
+    first_name = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
+    birthdate = serializers.DateField(allow_null=True, default=None)
+    is_employee = serializers.BooleanField(allow_null=True, default=False)
+
+    is_superuser = serializers.BooleanField(read_only=True)
+
+    def create(self, validated_data: dict):
+        if validated_data["is_employee"]:
+            return User.objects.create_superuser(**validated_data)
+
+        return User.objects.create_user(**validated_data)
